890755bab424a408805246014b426d736a4b0460
JAICHANGPARK <aristojeff@gmail.com>
2026-05-18 19:02:32 +0900

    Update local_services_app.dart

--- lib/ep2601_local_services_app/local_services_app.dart
+++ lib/ep2601_local_services_app/local_services_app.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'ui/local_services_main_page.dart';
 
-
 class LocalServicesApp extends StatelessWidget {
   const LocalServicesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-       home: LocalServicesMainPage();
-    );
+    return MaterialApp(home: LocalServicesMainPage());
   }
 }
