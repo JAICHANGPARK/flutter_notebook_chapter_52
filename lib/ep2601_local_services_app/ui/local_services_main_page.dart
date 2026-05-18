e2be8baf5d2c5d832e2d024a38e738fdf204d849
JAICHANGPARK <aristojeff@gmail.com>
2026-05-18 19:01:37 +0900

    Create local_services_main_page.dart

---
+++ lib/ep2601_local_services_app/ui/local_services_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LocalServicesMainPage extends StatefulWidget {
+  const LocalServicesMainPage({super.key});
+
+  @override
+  State<LocalServicesMainPage> createState() => _LocalServicesMainPageState();
+}
+
+class _LocalServicesMainPageState extends State<LocalServicesMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
