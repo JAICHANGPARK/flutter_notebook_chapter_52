6d7221d1f02112811f8185eecb97a224485d9bd9
JAICHANGPARK <aristojeff@gmail.com>
2026-05-18 19:01:13 +0900

    Update ep2601_main.dart

--- lib/ep2601_local_services_app/ep2601_main.dart
+++ lib/ep2601_local_services_app/ep2601_main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_52/ep2601_local_services_app/local_services_app.dart';
 
 
 void main() {
-
+  runApp(LocalServicesApp());
 
 }
