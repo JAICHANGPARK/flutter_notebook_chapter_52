412f3c2f6c1a0b0a33278551ebfd3db0976e9b5d
JAICHANGPARK <aristojeff@gmail.com>
2026-05-26 18:41:51 +0900

    Update local_services_bg_widget.dart

--- lib/ep2601_local_services_app/ui/widgets/local_services_bg_widget.dart
+++ lib/ep2601_local_services_app/ui/widgets/local_services_bg_widget.dart
@@ -6,6 +6,18 @@ class LocalServicesBgWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: .topCenter,
+          end: .bottomCenter,
+          colors: [
+            Color.fromRGBO(221, 239, 251, 1),
+            Color.fromRGBO(249, 249, 249, 1),
+            Color.fromRGBO(237, 243, 246, 1),
+          ],
+        ),
+      ),
+    );
   }
 }
