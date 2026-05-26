adacc3b91db986d4600439f0ac000722b45871ef
JAICHANGPARK <aristojeff@gmail.com>
2026-05-26 18:43:33 +0900

    Create local_services_home_page.dart

---
+++ lib/ep2601_local_services_app/ui/local_services_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocalServicesHomePage extends StatefulWidget {
+  const LocalServicesHomePage({super.key});
+
+  @override
+  State<LocalServicesHomePage> createState() => _LocalServicesHomePageState();
+}
+
+class _LocalServicesHomePageState extends State<LocalServicesHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
