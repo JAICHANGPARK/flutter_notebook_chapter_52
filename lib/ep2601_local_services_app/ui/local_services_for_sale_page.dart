d7d1b0b79ac42d077df198d9c432417cfa14c14d
JAICHANGPARK <aristojeff@gmail.com>
2026-05-26 18:43:06 +0900

    Update local_services_for_sale_page.dart

--- lib/ep2601_local_services_app/ui/local_services_for_sale_page.dart
+++ lib/ep2601_local_services_app/ui/local_services_for_sale_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/local_services_bg_widget.dart';
+
 
 class LocalServicesForSalePage extends StatefulWidget {
   const LocalServicesForSalePage({super.key});
@@ -11,6 +13,12 @@ class LocalServicesForSalePage extends StatefulWidget {
 class _LocalServicesForSalePageState extends State<LocalServicesForSalePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(child: LocalServicesBgWidget()),
+        ],
+      ),
+    );
   }
 }
