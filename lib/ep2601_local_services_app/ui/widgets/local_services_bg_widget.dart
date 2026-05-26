6490b33095e579f1790cbcae4dc2b8952b69631b
JAICHANGPARK <aristojeff@gmail.com>
2026-05-26 18:41:22 +0900

    Update local_services_bg_widget.dart

---
+++ lib/ep2601_local_services_app/ui/widgets/local_services_bg_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class LocalServicesBgWidget extends StatelessWidget {
+  const LocalServicesBgWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
