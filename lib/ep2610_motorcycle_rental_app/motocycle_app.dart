53d4bc8ba4ba3dae9ec1bf143b715396bb1263cb
JAICHANGPARK <aristojeff@gmail.com>
2026-06-01 18:54:53 +0900

    Update motocycle_app.dart

--- lib/ep2610_motorcycle_rental_app/motocycle_app.dart
+++ lib/ep2610_motorcycle_rental_app/motocycle_app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'ui/motocycle_home_pagep.dart';
+
 class MotorcycleApp extends StatelessWidget {
   const MotorcycleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: MotorcycleHomePage(),
 
     );
   }
