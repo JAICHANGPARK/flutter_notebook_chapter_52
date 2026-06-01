93d44f8af37834ceff6de21b96f02e1bb5f3cbc3
JAICHANGPARK <aristojeff@gmail.com>
2026-06-01 18:52:59 +0900

    Update ep2610_main.dart

--- lib/ep2610_motorcycle_rental_app/ep2610_main.dart
+++ lib/ep2610_motorcycle_rental_app/ep2610_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_52/ep2610_motorcycle_rental_app/motocycle_app.dart';
 
 
 void main() {
-
+  runApp(MotorcycleApp());
 }
