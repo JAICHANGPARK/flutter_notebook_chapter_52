59c886bb9c760e6254cd7af98a3f332fdc3258aa
JAICHANGPARK <aristojeff@gmail.com>
2026-06-07 10:50:04 +0900

    Create ai_freelance_app.dart

---
+++ lib/ep2616_ai_freelance_app/ai_freelance_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class AiFreelanceApp extends StatelessWidget {
+  const AiFreelanceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
