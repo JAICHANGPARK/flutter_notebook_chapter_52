06888953784fa2e2c7bead4ad134b89a0fb519c9
JAICHANGPARK <aristojeff@gmail.com>
2026-06-07 10:48:04 +0900

    Update ai_freelance_main_page.dart

--- lib/ep2616_ai_freelance_app/ui/ai_freelance_main_page.dart
+++ lib/ep2616_ai_freelance_app/ui/ai_freelance_main_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class AiFreelanceMainPage extends StatefulWidget {
+  const AiFreelanceMainPage({super.key});
+
+  @override
+  State<AiFreelanceMainPage> createState() => _AiFreelanceMainPageState();
+}
+
+class _AiFreelanceMainPageState extends State<AiFreelanceMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
