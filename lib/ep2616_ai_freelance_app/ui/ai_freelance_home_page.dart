ad88190fab9cb6e789a6b04d5e264fdefe68af29
JAICHANGPARK <aristojeff@gmail.com>
2026-06-08 18:52:34 +0900

    Update ai_freelance_home_page.dart

--- lib/ep2616_ai_freelance_app/ui/ai_freelance_home_page.dart
+++ lib/ep2616_ai_freelance_app/ui/ai_freelance_home_page.dart
@@ -11,59 +11,61 @@ class AiFreelanceHomePage extends StatefulWidget {
 class _AiFreelanceHomePageState extends State<AiFreelanceHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: .start,
-      spacing: 16,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            spacing: 8,
-            crossAxisAlignment: .start,
-            children: [
-              Text("Welcome back, Dreamwalker"),
-              Text("Ready to land your\nnext project?"),
-              Container(
-                height: 140,
-                child: Placeholder(),
-              ),
-            ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: .start,
+        spacing: 16,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              spacing: 8,
+              crossAxisAlignment: .start,
+              children: [
+                Text("Welcome back, Dreamwalker"),
+                Text("Ready to land your\nnext project?"),
+                Container(
+                  height: 140,
+                  child: Placeholder(),
+                ),
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
-            mainAxisAlignment: .spaceBetween,
-            children: [
-              Text("AI- Matched Jobs For You"),
-              Icon(Icons.arrow_forward)
-            ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: .spaceBetween,
+              children: [
+                Text("AI- Matched Jobs For You"),
+                Icon(Icons.arrow_forward)
+              ],
+            ),
           ),
-        ),
-
-
-        Container(
-          padding: .only(left: 16),
-          height: 240,
-          child: Placeholder(),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
-            mainAxisAlignment: .spaceBetween,
-            children: [
-              Text("My Services(5)"),
-              Icon(Icons.arrow_forward)
-            ],
+      
+      
+          Container(
+            padding: .only(left: 16),
+            height: 240,
+            child: Placeholder(),
           ),
-        ),
-        Container(
-          padding: .only(left: 16),
-          height: 240,
-          child: Placeholder(),
-        ),
-
-      ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: .spaceBetween,
+              children: [
+                Text("My Services(5)"),
+                Icon(Icons.arrow_forward)
+              ],
+            ),
+          ),
+          Container(
+            padding: .only(left: 16),
+            height: 240,
+            child: Placeholder(),
+          ),
+      
+        ],
+      ),
     );
   }
 }
