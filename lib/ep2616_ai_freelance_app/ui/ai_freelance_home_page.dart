07b0040c9497e319eb4d684716972e91f1dcbc9f
JAICHANGPARK <aristojeff@gmail.com>
2026-06-12 19:03:24 +0900

    Update ai_freelance_home_page.dart

--- lib/ep2616_ai_freelance_app/ui/ai_freelance_home_page.dart
+++ lib/ep2616_ai_freelance_app/ui/ai_freelance_home_page.dart
@@ -234,7 +234,7 @@ class _AiFreelanceHomePageState extends State<AiFreelanceHomePage> {
                                           ),
                                           child: Center(child: Text("Figma")),
                                         ),
-                                        Container(
+                                        Container( margin: .only(right: 8),
                                           padding: .symmetric(horizontal: 12),
                                           decoration: BoxDecoration(
                                             color: Colors.grey[100]!,
