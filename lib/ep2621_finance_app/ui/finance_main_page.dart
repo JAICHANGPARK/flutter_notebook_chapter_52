352f86b538ede072a8e7dc160911a6f4534a19dd
JAICHANGPARK <aristojeff@gmail.com>
2026-06-13 09:57:17 +0900

    Update finance_main_page.dart

--- lib/ep2621_finance_app/ui/finance_main_page.dart
+++ lib/ep2621_finance_app/ui/finance_main_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class FinanceMainPage extends StatefulWidget {
+  const FinanceMainPage({super.key});
+
+  @override
+  State<FinanceMainPage> createState() => _FinanceMainPageState();
+}
+
+class _FinanceMainPageState extends State<FinanceMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
