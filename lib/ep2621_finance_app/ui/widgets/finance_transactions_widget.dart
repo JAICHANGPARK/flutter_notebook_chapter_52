739af4ae931a39f6a2467387be76f139057f2106
JAICHANGPARK <aristojeff@gmail.com>
2026-06-17 19:00:22 +0900

    Update finance_transactions_widget.dart

--- lib/ep2621_finance_app/ui/widgets/finance_transactions_widget.dart
+++ lib/ep2621_finance_app/ui/widgets/finance_transactions_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 
 class FinanceTransactionsWidget extends StatelessWidget {
@@ -6,6 +7,62 @@ class FinanceTransactionsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      spacing: 4,
+      crossAxisAlignment: .start,
+      children: [
+        Row(
+          mainAxisAlignment: .spaceBetween,
+          children: [
+            Text(
+              "Transactions",
+              style: TextStyle(fontWeight: .bold, fontSize: 18),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text("View All"),
+              style: TextButton.styleFrom(foregroundColor: Colors.black),
+            ),
+          ],
+        ),
+
+        Text("Today"),
+        Gap(4),
+        Column(
+          spacing: 12,
+          children: List.generate(5, (idx) {
+            return Container(
+              // height: 52,
+              decoration: ShapeDecoration(
+                shape: StadiumBorder(),
+                color: Colors.white,
+              ),
+              padding: .symmetric(horizontal: 16, vertical: 12),
+              child: Row(
+                spacing: 12,
+                children: [
+                  CircleAvatar(radius: 24),
+                  Expanded(
+                    child: Column(
+                      spacing: 4,
+                      children: [
+                        Row(
+                          mainAxisAlignment: .spaceBetween,
+                          children: [Text("Grocery"), Text("- \$12.99")],
+                        ),
+                        Row(
+                          mainAxisAlignment: .spaceBetween,
+                          children: [Text("Payment"), Text("10:32 am")],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }),
+        ),
+      ],
+    );
   }
 }
