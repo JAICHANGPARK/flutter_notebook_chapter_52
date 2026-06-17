cee1ae606f5f12f730a8141c01075f2151366191
JAICHANGPARK <aristojeff@gmail.com>
2026-06-17 18:52:48 +0900

    Create finance_details_page.dart

---
+++ lib/ep2621_finance_app/ui/finance_details_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class FinanceDetailsPage extends StatefulWidget {
+  const FinanceDetailsPage({super.key});
+
+  @override
+  State<FinanceDetailsPage> createState() => _FinanceDetailsPageState();
+}
+
+class _FinanceDetailsPageState extends State<FinanceDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
