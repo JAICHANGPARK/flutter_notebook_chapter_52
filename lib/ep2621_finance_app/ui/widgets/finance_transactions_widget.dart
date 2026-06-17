35a27ec66a01d0e32408f0d21abb4ba465a566b7
JAICHANGPARK <aristojeff@gmail.com>
2026-06-17 18:59:50 +0900

    Create finance_transactions_widget.dart

---
+++ lib/ep2621_finance_app/ui/widgets/finance_transactions_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FinanceTransactionsWidget extends StatelessWidget {
+  const FinanceTransactionsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
