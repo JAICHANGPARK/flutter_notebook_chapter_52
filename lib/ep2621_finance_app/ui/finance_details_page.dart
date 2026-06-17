86e7d5753bcd0e33d9ac2b7e0b7becd34b4d83ad
JAICHANGPARK <aristojeff@gmail.com>
2026-06-17 18:56:00 +0900

    Update finance_details_page.dart

--- lib/ep2621_finance_app/ui/finance_details_page.dart
+++ lib/ep2621_finance_app/ui/finance_details_page.dart
@@ -12,6 +12,16 @@ class _FinanceDetailsPageState extends State<FinanceDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(child: Column(children: [
+
+        Row(
+          children: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.keyboard_arrow_left)),
+            Expanded(child: Center(child: Text("Account Details"),))
+          ],
+        )
+
+      ],)),
 
     );
   }
