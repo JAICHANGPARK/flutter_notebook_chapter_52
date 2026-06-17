dff9f34b4cf42e99f3ba559e059a3b21952e6407
JAICHANGPARK <aristojeff@gmail.com>
2026-06-17 19:00:44 +0900

    Update finance_transactions_widget.dart

--- lib/ep2621_finance_app/ui/widgets/finance_transactions_widget.dart
+++ lib/ep2621_finance_app/ui/widgets/finance_transactions_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-
 class FinanceTransactionsWidget extends StatelessWidget {
   const FinanceTransactionsWidget({super.key});
 
