34259de4db4d0d2cbd42ba6343e41e91f5dcf615
JAICHANGPARK <aristojeff@gmail.com>
2026-06-25 18:38:04 +0900

    Update marketplace_home_page.dart

--- lib/ep2631_marketplace_app/ui/marketplace_home_page.dart
+++ lib/ep2631_marketplace_app/ui/marketplace_home_page.dart
@@ -10,6 +10,27 @@ class MarketplaceHomePage extends StatefulWidget {
 class _MarketplaceHomePageState extends State<MarketplaceHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(child: Column()),
+          Positioned(
+              bottom: 32,
+              left: 0,
+              right: 0,
+              child: Container(
+            height: 72,
+                child: Row(
+                  mainAxisAlignment: .center,
+                  children: [
+                    Container(
+                      decoration: ShapeDecoration(shape: StadiumBorder()),
+                    )
+                  ],
+                ),
+          ))
+        ],
+      ),
+    );
   }
 }
