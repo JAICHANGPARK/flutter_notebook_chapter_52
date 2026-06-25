3a73beea3312ed33d9824a7895725d5da2e445d5
JAICHANGPARK <aristojeff@gmail.com>
2026-06-25 18:36:12 +0900

    Update ep2631_main.dart

--- lib/ep2631_marketplace_app/ep2631_main.dart
+++ lib/ep2631_marketplace_app/ep2631_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_52/ep2631_marketplace_app/ui/marketplace_app.dart';
 
 
 void main() {
-  
+  runApp(MarketplaceApp());
 }
