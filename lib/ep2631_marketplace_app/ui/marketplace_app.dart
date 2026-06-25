b92cd1e625f10e2d53f76879e12265980990ffe8
JAICHANGPARK <aristojeff@gmail.com>
2026-06-25 18:33:31 +0900

    Update marketplace_app.dart

---
+++ lib/ep2631_marketplace_app/ui/marketplace_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class MarketplaceApp extends StatelessWidget {
+  const MarketplaceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
