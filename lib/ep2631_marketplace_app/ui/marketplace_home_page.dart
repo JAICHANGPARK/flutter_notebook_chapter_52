bbaeed727a1a52527fe00ea37770d46dc987ed2e
JAICHANGPARK <aristojeff@gmail.com>
2026-06-25 18:34:24 +0900

    Create marketplace_home_page.dart

---
+++ lib/ep2631_marketplace_app/ui/marketplace_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class MarketplaceHomePage extends StatefulWidget {
+  const MarketplaceHomePage({super.key});
+
+  @override
+  State<MarketplaceHomePage> createState() => _MarketplaceHomePageState();
+}
+
+class _MarketplaceHomePageState extends State<MarketplaceHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
