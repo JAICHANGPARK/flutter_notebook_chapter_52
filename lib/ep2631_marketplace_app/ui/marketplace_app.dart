51980b8de64bd64d500ba44b01b79942917f86b4
Dreamwalker <aristojeff@gmail.com>
2026-06-26 12:05:00 +0900

    chore: format marketplace UI files

--- lib/ep2631_marketplace_app/ui/marketplace_app.dart
+++ lib/ep2631_marketplace_app/ui/marketplace_app.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_52/ep2631_marketplace_app/ui/marketplace_home_page.dart';
 
-
 class MarketplaceApp extends StatelessWidget {
   const MarketplaceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: MarketplaceHomePage(),
-    );
+    return MaterialApp(home: MarketplaceHomePage());
   }
 }
--- lib/ep2631_marketplace_app/ui/marketplace_home_page.dart
+++ lib/ep2631_marketplace_app/ui/marketplace_home_page.dart
@@ -27,10 +27,7 @@ class _MarketplaceHomePageState extends State<MarketplaceHomePage> {
                     decoration: ShapeDecoration(shape: StadiumBorder()),
                     child: Row(
                       spacing: 4,
-                      children: [
-                        Icon(Icons.home_filled),
-                        Text("Home")
-                      ],
+                      children: [Icon(Icons.home_filled), Text("Home")],
                     ),
                   ),
                   IconButton(onPressed: () {}, icon: Icon(Icons.search)),
