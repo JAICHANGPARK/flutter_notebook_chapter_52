d153bd241dce33e54a4be7120e5dd4f0d0b34070
JAICHANGPARK <aristojeff@gmail.com>
2026-07-02 18:55:18 +0900

    Update booking_start_page.dart

--- lib/ep2636_travel_hotel_booking_app/ui/booking_start_page.dart
+++ lib/ep2636_travel_hotel_booking_app/ui/booking_start_page.dart
@@ -5,6 +5,10 @@ class BookingStartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Stack());
+    return Scaffold(body: Stack(
+      children: [
+        
+      ],
+    ));
   }
 }
