3225e9711fd19f561ddbd63e474ab39da666c132
JAICHANGPARK <aristojeff@gmail.com>
2026-07-02 18:50:07 +0900

    Update ep2636_main.dart

--- lib/ep2636_travel_hotel_booking_app/ep2636_main.dart
+++ lib/ep2636_travel_hotel_booking_app/ep2636_main.dart
@@ -1,6 +1,9 @@
 
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_notebook_chapter_52/ep2636_travel_hotel_booking_app/booking_app.dart';
+
 void main() {
 
-  
+  runApp(BookingApp());
 }
