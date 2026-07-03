d74e06b475a89f65196df1a0b363a7150200f49c
JAICHANGPARK <aristojeff@gmail.com>
2026-07-03 18:22:34 +0900

    Update booking_home_page.dart

--- lib/ep2636_travel_hotel_booking_app/ui/booking_home_page.dart
+++ lib/ep2636_travel_hotel_booking_app/ui/booking_home_page.dart
@@ -12,7 +12,9 @@ class _BookingHomePageState extends State<BookingHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(241, 245, 245, 1),
-      body: Stack(children: []),
+      body: Stack(children: [
+
+      ]),
     );
   }
 }
