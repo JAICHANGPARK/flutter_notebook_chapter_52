de58a6b3958839e9e015abaac2c3ca0eb6668ab3
JAICHANGPARK <aristojeff@gmail.com>
2026-07-07 18:56:44 +0900

    Update ai_assistants_main_page.dart

--- lib/ep2641_ai_assistants_app/ui/ai_assistants_main_page.dart
+++ lib/ep2641_ai_assistants_app/ui/ai_assistants_main_page.dart
@@ -10,6 +10,6 @@ class AiAssistantsMainPage extends StatefulWidget {
 class _AiAssistantsMainPageState extends State<AiAssistantsMainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
