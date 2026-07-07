97f74fda088f06d3d2de3b2a1dd51314427b63c1
JAICHANGPARK <aristojeff@gmail.com>
2026-07-07 18:54:05 +0900

    Update ai_assistants_app.dart

--- lib/ep2641_ai_assistants_app/ai_assistants_app.dart
+++ lib/ep2641_ai_assistants_app/ai_assistants_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_52/ep2641_ai_assistants_app/ui/ai_assistants_main_page.dart';
 
 
 class AiAssistantsApp extends StatelessWidget {
@@ -6,6 +7,8 @@ class AiAssistantsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: AiAssistantsMainPage(),
+    );
   }
 }
