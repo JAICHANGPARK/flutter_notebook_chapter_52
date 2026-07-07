d5944f7b7ec6f443035a0c19d8873297bd1c7f1f
JAICHANGPARK <aristojeff@gmail.com>
2026-07-07 18:52:41 +0900

    Update ai_assistants_main_page.dart

--- lib/ep2641_ai_assistants_app/ui/ai_assistants_main_page.dart
+++ lib/ep2641_ai_assistants_app/ui/ai_assistants_main_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class AiAssistantsMainPage extends StatefulWidget {
+  const AiAssistantsMainPage({super.key});
+
+  @override
+  State<AiAssistantsMainPage> createState() => _AiAssistantsMainPageState();
+}
+
+class _AiAssistantsMainPageState extends State<AiAssistantsMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
