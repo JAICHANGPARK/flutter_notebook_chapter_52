0125233945273c50554e6d32fa77e09742dc5a08
JAICHANGPARK <aristojeff@gmail.com>
2026-07-07 18:56:53 +0900

    Update ep2641_main.dart

--- lib/ep2641_ai_assistants_app/ep2641_main.dart
+++ lib/ep2641_ai_assistants_app/ep2641_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_52/ep2641_ai_assistants_app/ai_assistants_app.dart';
 
 
 void main() {
-
+  runApp(AiAssistantsApp());
 }
