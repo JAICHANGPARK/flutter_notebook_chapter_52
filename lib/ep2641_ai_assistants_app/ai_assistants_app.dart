a6bcb6ee6220552c97873d43c6cbda8807c77b46
JAICHANGPARK <aristojeff@gmail.com>
2026-07-07 18:52:58 +0900

    Create ai_assistants_app.dart

---
+++ lib/ep2641_ai_assistants_app/ai_assistants_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class AiAssistantsApp extends StatelessWidget {
+  const AiAssistantsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
