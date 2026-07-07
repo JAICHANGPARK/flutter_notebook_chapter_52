08fd7984aebd1d3c31f2e0955a41813e919d24c8
JAICHANGPARK <aristojeff@gmail.com>
2026-07-07 18:52:18 +0900

    Create ai_assistants_chat_page.dart

---
+++ lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AiAssistantsChatPage extends StatefulWidget {
+  const AiAssistantsChatPage({super.key});
+
+  @override
+  State<AiAssistantsChatPage> createState() => _AiAssistantsChatPageState();
+}
+
+class _AiAssistantsChatPageState extends State<AiAssistantsChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
