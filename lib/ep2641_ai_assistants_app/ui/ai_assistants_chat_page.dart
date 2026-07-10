4626ada7b61ab92d151f4c39d6eb5e62152ff6d5
JAICHANGPARK <aristojeff@gmail.com>
2026-07-10 19:26:16 +0900

    Update ai_assistants_chat_page.dart

--- lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
+++ lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
@@ -17,51 +17,56 @@ class _AiAssistantsChatPageState extends State<AiAssistantsChatPage> {
         Text(
           "Hello dream! i'm your AI Friend Assistant. How can I help you with Oliver today?",
         ),
-        Container(
-          padding: .all(8),
-          decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
-          child: Row(
-            spacing: 12,
-            mainAxisSize: .min,
-            children: [Icon(Icons.pets), Text("Why is Oliver scratching?")],
-          ),
-        ),
-        Container(
-          padding: .all(8),
-          decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
-          child: Row(
-            spacing: 12,
-            mainAxisSize: .min,
-            children: [Icon(Icons.pets), Text("Why is Oliver scratching?")],
-          ),
-        ),
-        Container(
-          padding: .all(8),
-          decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
-          child: Row(
-            spacing: 12,
-            mainAxisSize: .min,
-            children: [Icon(Icons.medication), Text("What vaccines does Oliver need?")],
-          ),
-        ),
-        Container(
-          padding: .all(8),
-          decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
-          child: Row(
-            spacing: 12,
-            mainAxisSize: .min,
-            children: [Icon(Icons.fastfood_outlined), Text("Can Oliver eat human food?")],
-          ),
-        ),
-        Container(
-          padding: .all(8),
-          decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
-          child: Row(
-            spacing: 12,
-            mainAxisSize: .min,
-            children: [Icon(Icons.pets), Text("Why is Oliver vomiting")],
-          ),
-        ),
+        Column(
+          children: [
+            Container(
+              padding: .all(8),
+              decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
+              child: Row(
+                spacing: 12,
+                mainAxisSize: .min,
+                children: [Icon(Icons.pets), Text("Why is Oliver scratching?")],
+              ),
+            ),
+            Container(
+              padding: .all(8),
+              decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
+              child: Row(
+                spacing: 12,
+                mainAxisSize: .min,
+                children: [Icon(Icons.pets), Text("Why is Oliver scratching?")],
+              ),
+            ),
+            Container(
+              padding: .all(8),
+              decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
+              child: Row(
+                spacing: 12,
+                mainAxisSize: .min,
+                children: [Icon(Icons.medication), Text("What vaccines does Oliver need?")],
+              ),
+            ),
+            Container(
+              padding: .all(8),
+              decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
+              child: Row(
+                spacing: 12,
+                mainAxisSize: .min,
+                children: [Icon(Icons.fastfood_outlined), Text("Can Oliver eat human food?")],
+              ),
+            ),
+            Container(
+              padding: .all(8),
+              decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
+              child: Row(
+                spacing: 12,
+                mainAxisSize: .min,
+                children: [Icon(Icons.pets), Text("Why is Oliver vomiting")],
+              ),
+            ),
+          ],
+        )
+
       ],
     );
   }
