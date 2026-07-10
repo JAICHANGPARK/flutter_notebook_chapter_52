b5982a856ba3eadfc40613eed17f4535ea043b91
JAICHANGPARK <aristojeff@gmail.com>
2026-07-10 19:20:20 +0900

    Update ai_assistants_chat_page.dart

--- lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
+++ lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
@@ -16,6 +16,10 @@ class _AiAssistantsChatPageState extends State<AiAssistantsChatPage> {
         Text(
           "Hello dream! i'm your AI Friend Assistant. How can I help you with Oliver today?",
         ),
+        Container(
+          decoration: ShapeDecoration(shape: StadiumBorder()),
+          child: Row(children: []),
+        ),
       ],
     );
   }
