c31ee289df4e6d9ad05aefccaf32cf471a308e86
JAICHANGPARK <aristojeff@gmail.com>
2026-07-10 19:21:28 +0900

    Update ai_assistants_chat_page.dart

--- lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
+++ lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
@@ -17,7 +17,9 @@ class _AiAssistantsChatPageState extends State<AiAssistantsChatPage> {
           "Hello dream! i'm your AI Friend Assistant. How can I help you with Oliver today?",
         ),
         Container(
-          decoration: ShapeDecoration(shape: StadiumBorder()),
+          decoration: ShapeDecoration(shape: StadiumBorder(
+            side: BorderSide()
+          ), ),
           child: Row(children: [
             Icon(Icons.pets),
             Text("Why is Oliver scratching?")
