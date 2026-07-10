86cae63257e8ba2fc45b3f12bfc410c1f20256e0
JAICHANGPARK <aristojeff@gmail.com>
2026-07-10 19:28:17 +0900

    Update ai_assistants_chat_page.dart

--- lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
+++ lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
@@ -30,7 +30,7 @@ class _AiAssistantsChatPageState extends State<AiAssistantsChatPage> {
             spacing: 12,
             children: [
               Container(
-                padding: .all(8),
+                padding: .symmetric(horizontal: 16),
                 decoration: ShapeDecoration(
                   shape: StadiumBorder(side: BorderSide(
                     width: 2
