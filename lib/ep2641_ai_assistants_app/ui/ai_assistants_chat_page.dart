db4dcbd484ef36678e1950b1c22708729a3f4fd0
JAICHANGPARK <aristojeff@gmail.com>
2026-07-10 19:26:43 +0900

    Update ai_assistants_chat_page.dart

--- lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
+++ lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
@@ -11,63 +11,68 @@ class _AiAssistantsChatPageState extends State<AiAssistantsChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("AI Chat"),
-        Text(
-          "Hello dream! i'm your AI Friend Assistant. How can I help you with Oliver today?",
-        ),
-        Column(
-          children: [
-            Container(
-              padding: .all(8),
-              decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
-              child: Row(
-                spacing: 12,
-                mainAxisSize: .min,
-                children: [Icon(Icons.pets), Text("Why is Oliver scratching?")],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 64),
+      child: Column(
+        mainAxisAlignment: .center,
+        spacing: 12,
+        children: [
+          Text("AI Chat"),
+          Text(
+            "Hello dream! i'm your AI Friend Assistant. How can I help you with Oliver today?",
+          ),
+          Column(
+            children: [
+              Container(
+                padding: .all(8),
+                decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
+                child: Row(
+                  spacing: 12,
+                  mainAxisSize: .min,
+                  children: [Icon(Icons.pets), Text("Why is Oliver scratching?")],
+                ),
               ),
-            ),
-            Container(
-              padding: .all(8),
-              decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
-              child: Row(
-                spacing: 12,
-                mainAxisSize: .min,
-                children: [Icon(Icons.pets), Text("Why is Oliver scratching?")],
+              Container(
+                padding: .all(8),
+                decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
+                child: Row(
+                  spacing: 12,
+                  mainAxisSize: .min,
+                  children: [Icon(Icons.pets), Text("Why is Oliver scratching?")],
+                ),
               ),
-            ),
-            Container(
-              padding: .all(8),
-              decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
-              child: Row(
-                spacing: 12,
-                mainAxisSize: .min,
-                children: [Icon(Icons.medication), Text("What vaccines does Oliver need?")],
+              Container(
+                padding: .all(8),
+                decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
+                child: Row(
+                  spacing: 12,
+                  mainAxisSize: .min,
+                  children: [Icon(Icons.medication), Text("What vaccines does Oliver need?")],
+                ),
               ),
-            ),
-            Container(
-              padding: .all(8),
-              decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
-              child: Row(
-                spacing: 12,
-                mainAxisSize: .min,
-                children: [Icon(Icons.fastfood_outlined), Text("Can Oliver eat human food?")],
+              Container(
+                padding: .all(8),
+                decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
+                child: Row(
+                  spacing: 12,
+                  mainAxisSize: .min,
+                  children: [Icon(Icons.fastfood_outlined), Text("Can Oliver eat human food?")],
+                ),
               ),
-            ),
-            Container(
-              padding: .all(8),
-              decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
-              child: Row(
-                spacing: 12,
-                mainAxisSize: .min,
-                children: [Icon(Icons.pets), Text("Why is Oliver vomiting")],
+              Container(
+                padding: .all(8),
+                decoration: ShapeDecoration(shape: StadiumBorder(side: BorderSide())),
+                child: Row(
+                  spacing: 12,
+                  mainAxisSize: .min,
+                  children: [Icon(Icons.pets), Text("Why is Oliver vomiting")],
+                ),
               ),
-            ),
-          ],
-        )
+            ],
+          )
 
-      ],
+        ],
+      ),
     );
   }
 }
