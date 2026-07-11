0f390a2cc9b6adc646ab12169bcec956545e4038
JAICHANGPARK <aristojeff@gmail.com>
2026-07-11 10:36:36 +0900

    Update ai_assistants_chat_page.dart

--- lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
+++ lib/ep2641_ai_assistants_app/ui/ai_assistants_chat_page.dart
@@ -11,101 +11,107 @@ class AiAssistantsChatPage extends StatefulWidget {
 class _AiAssistantsChatPageState extends State<AiAssistantsChatPage> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 64),
-      child: Column(
-        mainAxisAlignment: .center,
-        spacing: 12,
-        children: [
-          Text("AI Chat", style: TextStyle(fontWeight: .bold, fontSize: 18)),
-          Gap(3),
-          Text(
-            "Hello dream! i'm your AI Friend Assistant. How can I help you with Oliver today?",
-            textAlign: .center,
-            style: TextStyle(fontWeight: .bold),
-          ),
-          Gap(8),
-
-          Column(
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 64),
+          child: Column(
+            mainAxisAlignment: .center,
             spacing: 12,
             children: [
-              Container(
-                padding: .symmetric(horizontal: 16, vertical: 8),
-                decoration: ShapeDecoration(
-                  color: Color.fromRGBO(250, 246, 222, 1),
-                  shape: StadiumBorder(side: BorderSide(width: 2)),
-                ),
-                child: Row(
-                  spacing: 12,
-                  mainAxisSize: .min,
-                  children: [
-                    Icon(Icons.pets),
-                    Text(
-                      "Why is Oliver scratching?",
-                      style: TextStyle(fontSize: 12),
-                    ),
-                  ],
-                ),
-              ),
 
-              Container(
-                padding: .symmetric(horizontal: 16, vertical: 8),
-                decoration: ShapeDecoration(
-                  color: Color.fromRGBO(250, 246, 222, 1),
-                  shape: StadiumBorder(side: BorderSide(width: 2)),
-                ),
-                child: Row(
-                  spacing: 12,
-                  mainAxisSize: .min,
-                  children: [
-                    Icon(Icons.medication),
-                    Text(
-                      "What vaccines does Oliver need?",
-                      style: TextStyle(fontSize: 12, color: Colors.black87),
-                    ),
-                  ],
-                ),
+
+              Text("AI Chat", style: TextStyle(fontWeight: .bold, fontSize: 18)),
+              Gap(3),
+              Text(
+                "Hello dream! i'm your AI Friend Assistant. How can I help you with Oliver today?",
+                textAlign: .center,
+                style: TextStyle(fontWeight: .bold),
               ),
-              Container(
-                padding: .symmetric(horizontal: 16, vertical: 8),
-                decoration: ShapeDecoration(
-                  color: Color.fromRGBO(250, 246, 222, 1),
-                  shape: StadiumBorder(side: BorderSide(width: 2)),
-                ),
-                child: Row(
-                  spacing: 12,
-                  mainAxisSize: .min,
-                  children: [
-                    Icon(Icons.fastfood_outlined),
-                    Text(
-                      "Can Oliver eat human food?",
-                      style: TextStyle(fontSize: 12, color: Colors.black87),
+              Gap(8),
+
+              Column(
+                spacing: 12,
+                children: [
+                  Container(
+                    padding: .symmetric(horizontal: 16, vertical: 8),
+                    decoration: ShapeDecoration(
+                      color: Color.fromRGBO(250, 246, 222, 1),
+                      shape: StadiumBorder(side: BorderSide(width: 2)),
                     ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: .symmetric(horizontal: 16, vertical: 8),
-                decoration: ShapeDecoration(
-                  color: Color.fromRGBO(250, 246, 222, 1),
-                  shape: StadiumBorder(side: BorderSide(width: 2)),
-                ),
-                child: Row(
-                  spacing: 12,
-                  mainAxisSize: .min,
-                  children: [
-                    Icon(Icons.pets),
-                    Text(
-                      "Why is Oliver vomiting",
-                      style: TextStyle(fontSize: 12, color: Colors.black87),
+                    child: Row(
+                      spacing: 12,
+                      mainAxisSize: .min,
+                      children: [
+                        Icon(Icons.pets),
+                        Text(
+                          "Why is Oliver scratching?",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+
+                  Container(
+                    padding: .symmetric(horizontal: 16, vertical: 8),
+                    decoration: ShapeDecoration(
+                      color: Color.fromRGBO(250, 246, 222, 1),
+                      shape: StadiumBorder(side: BorderSide(width: 2)),
+                    ),
+                    child: Row(
+                      spacing: 12,
+                      mainAxisSize: .min,
+                      children: [
+                        Icon(Icons.medication),
+                        Text(
+                          "What vaccines does Oliver need?",
+                          style: TextStyle(fontSize: 12, color: Colors.black87),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: .symmetric(horizontal: 16, vertical: 8),
+                    decoration: ShapeDecoration(
+                      color: Color.fromRGBO(250, 246, 222, 1),
+                      shape: StadiumBorder(side: BorderSide(width: 2)),
+                    ),
+                    child: Row(
+                      spacing: 12,
+                      mainAxisSize: .min,
+                      children: [
+                        Icon(Icons.fastfood_outlined),
+                        Text(
+                          "Can Oliver eat human food?",
+                          style: TextStyle(fontSize: 12, color: Colors.black87),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: .symmetric(horizontal: 16, vertical: 8),
+                    decoration: ShapeDecoration(
+                      color: Color.fromRGBO(250, 246, 222, 1),
+                      shape: StadiumBorder(side: BorderSide(width: 2)),
+                    ),
+                    child: Row(
+                      spacing: 12,
+                      mainAxisSize: .min,
+                      children: [
+                        Icon(Icons.pets),
+                        Text(
+                          "Why is Oliver vomiting",
+                          style: TextStyle(fontSize: 12, color: Colors.black87),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
