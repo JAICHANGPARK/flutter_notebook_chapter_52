0f9c7b821cf38d18b552801d2b38800f4e1df9af
JAICHANGPARK <aristojeff@gmail.com>
2026-07-12 10:08:14 +0900

    Update expense_splitting_main_page.dart

--- lib/ep2645_expense_splitting_app/ui/expense_splitting_main_page.dart
+++ lib/ep2645_expense_splitting_app/ui/expense_splitting_main_page.dart
@@ -4,26 +4,34 @@ class ExpenseSplittingMainPage extends StatefulWidget {
   const ExpenseSplittingMainPage({super.key});
 
   @override
-  State<ExpenseSplittingMainPage> createState() => _ExpenseSplittingMainPageState();
+  State<ExpenseSplittingMainPage> createState() =>
+      _ExpenseSplittingMainPageState();
 }
 
 class _ExpenseSplittingMainPageState extends State<ExpenseSplittingMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-
-
+      body: Column(children: []),
+      bottomNavigationBar: BottomNavigationBar(
+        type: .fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box_outlined),
+            label: "Add Expense",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.wallet), label: "Balance"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: "Statistics",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: "Profile",
+          ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home_filled),label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.add_box_outlined),label: "Add Expense"),
-        BottomNavigationBarItem(icon: Icon(Icons.wallet),label: "Balance"),
-        BottomNavigationBarItem(icon: Icon(Icons.bar_chart),label: "Statistics"),
-        BottomNavigationBarItem(icon: Icon(Icons.add_box_outlined),label: "Profile"),
-      ],),
     );
   }
 }
