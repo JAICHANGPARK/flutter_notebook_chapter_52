1a887ec2fc8735351d212aba74aa0cd8e0090560
JAICHANGPARK <aristojeff@gmail.com>
2026-07-12 10:04:44 +0900

    Update expense_splitting_main_page.dart

---
+++ lib/ep2645_expense_splitting_app/ui/expense_splitting_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExpenseSplittingMainPage extends StatefulWidget {
+  const ExpenseSplittingMainPage({super.key});
+
+  @override
+  State<ExpenseSplittingMainPage> createState() => _ExpenseSplittingMainPageState();
+}
+
+class _ExpenseSplittingMainPageState extends State<ExpenseSplittingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
