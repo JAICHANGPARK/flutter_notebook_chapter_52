ed9f5728bd7fb24a09857dd5182859192d6e9fe1
JAICHANGPARK <aristojeff@gmail.com>
2026-07-12 10:02:59 +0900

    Create expense_splitting_home_page.dart

---
+++ lib/ep2645_expense_splitting_app/ui/expense_splitting_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ExpenseSplittingHomePage extends StatefulWidget {
+  const ExpenseSplittingHomePage({super.key});
+
+  @override
+  State<ExpenseSplittingHomePage> createState() => _ExpenseSplittingHomePageState();
+}
+
+class _ExpenseSplittingHomePageState extends State<ExpenseSplittingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
