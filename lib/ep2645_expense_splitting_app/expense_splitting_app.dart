5618ef432bceb07d280cddc5606ba117d6b19074
JAICHANGPARK <aristojeff@gmail.com>
2026-07-12 10:00:42 +0900

    Update expense_splitting_app.dart

---
+++ lib/ep2645_expense_splitting_app/expense_splitting_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ExpenseSplittingApp extends StatelessWidget {
+  const ExpenseSplittingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
