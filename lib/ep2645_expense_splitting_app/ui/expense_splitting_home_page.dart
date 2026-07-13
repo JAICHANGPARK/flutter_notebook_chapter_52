6e1a2199a7591600801544af9b1d86ac9f0381dc
JAICHANGPARK <aristojeff@gmail.com>
2026-07-13 18:45:43 +0900

    Update expense_splitting_home_page.dart

--- lib/ep2645_expense_splitting_app/ui/expense_splitting_home_page.dart
+++ lib/ep2645_expense_splitting_app/ui/expense_splitting_home_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 class ExpenseSplittingHomePage extends StatefulWidget {
   const ExpenseSplittingHomePage({super.key});
 
   @override
-  State<ExpenseSplittingHomePage> createState() => _ExpenseSplittingHomePageState();
+  State<ExpenseSplittingHomePage> createState() =>
+      _ExpenseSplittingHomePageState();
 }
 
 class _ExpenseSplittingHomePageState extends State<ExpenseSplittingHomePage> {
@@ -14,7 +14,12 @@ class _ExpenseSplittingHomePageState extends State<ExpenseSplittingHomePage> {
     return Scaffold(
       body: Column(
         children: [
-
+          Row(
+            children: [
+              Text("Home"),
+              CircleAvatar(backgroundColor: Colors.white),
+            ],
+          ),
         ],
       ),
     );
