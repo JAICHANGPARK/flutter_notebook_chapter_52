07f686bbaa4f1a6a154252bee4f5bd1d47e135b6
JAICHANGPARK <aristojeff@gmail.com>
2026-07-19 10:11:39 +0900

    Update expense_splitting_detail_page.dart

--- lib/ep2645_expense_splitting_app/ui/expense_splitting_detail_page.dart
+++ lib/ep2645_expense_splitting_app/ui/expense_splitting_detail_page.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 
-
 class ExpenseSplittingDetailPage extends StatefulWidget {
   const ExpenseSplittingDetailPage({super.key});
 
   @override
-  State<ExpenseSplittingDetailPage> createState() => _ExpenseSplittingDetailPageState();
+  State<ExpenseSplittingDetailPage> createState() =>
+      _ExpenseSplittingDetailPageState();
 }
 
-class _ExpenseSplittingDetailPageState extends State<ExpenseSplittingDetailPage> {
+class _ExpenseSplittingDetailPageState
+    extends State<ExpenseSplittingDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(247, 247, 247, 1),
-      appBar: AppBar(
-        title: Text("Roommate"),
-      ),
+      appBar: AppBar(title: Text("Roommate")),
       body: Column(
+        crossAxisAlignment: .start,
         children: [
-
-
+          Container(decoration: BoxDecoration(color: Colors.white)),
+          Container(decoration: BoxDecoration(color: Colors.white)),
         ],
       ),
     );
