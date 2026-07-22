c933df3a2e635df7b1b7bac326df1e7f88c67e82
JAICHANGPARK <aristojeff@gmail.com>
2026-07-22 18:26:34 +0900

    Update expense_add_page.dart

--- lib/ep2645_expense_splitting_app/ui/expense_add_page.dart
+++ lib/ep2645_expense_splitting_app/ui/expense_add_page.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+
+class ExpenseAddPage extends StatefulWidget {
+  const ExpenseAddPage({super.key});
+
+  @override
+  State<ExpenseAddPage> createState() => _ExpenseAddPageState();
+}
+
+class _ExpenseAddPageState extends State<ExpenseAddPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
