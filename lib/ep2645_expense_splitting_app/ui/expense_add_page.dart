34fdd437a2d16a510c59266ed5a2149238d13b90
JAICHANGPARK <aristojeff@gmail.com>
2026-07-22 18:36:16 +0900

    Update expense_add_page.dart

--- lib/ep2645_expense_splitting_app/ui/expense_add_page.dart
+++ lib/ep2645_expense_splitting_app/ui/expense_add_page.dart
@@ -14,6 +14,7 @@ class _ExpenseAddPageState extends State<ExpenseAddPage> {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          spacing: 24,
           crossAxisAlignment: .start,
           children: [
             Text(
